afacc4e19ddd353e210f109b520d3ffbab141a49
Nandadulal <nandadulal@gmail.com>
2022-03-25 19:10:21 +0530

    added git demo project to local repo

---
+++ src/main/java/com/nandadulal/JavaTechie_Git_25_March/App.java
@@ -0,0 +1,17 @@
+package com.nandadulal.JavaTechie_Git_25_March;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void addData() {
+    	System.out.println("Data added");
+    }
+}
