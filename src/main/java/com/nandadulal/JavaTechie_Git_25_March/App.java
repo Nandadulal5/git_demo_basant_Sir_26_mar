e865dfb153f10f776bacf182c683d2557a6ab64a
Nandadulal5 <84185091+Nandadulal5@users.noreply.github.com>
2022-04-06 09:09:19 +0530

    updated by developer 2

--- src/main/java/com/nandadulal/JavaTechie_Git_25_March/App.java
+++ src/main/java/com/nandadulal/JavaTechie_Git_25_March/App.java
@@ -12,6 +12,6 @@ public class App
     }
     
     public void addData() {
-    	System.out.println("Data added successfully again ok");
+    	System.out.println("Data added successfully again ok from remote");
     }
 }
